5c711a1848ddb75ac81918220b346023c4605dfc
FaizanAsifB <faizanasif.dev@gmail.com>
2024-11-07 19:17:34 +0100

    add about page

---
+++ src/app/(landing)/about/page.tsx
@@ -0,0 +1,12 @@
+const About = () => {
+  return (
+    <>
+      <section>Our Story</section>
+      <section>Mission & Vision</section>
+      <section>Core Values</section>
+      <section>Out Team</section>
+      <section>Our Approach</section>
+    </>
+  )
+}
+export default About
--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -4,32 +4,30 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <>
-      <section>
-        <div className="container p-20 flex items-center justify-between">
-          <div className="max-w-lg">
-            <h1 className="text-5xl text-pretty font-bold">
-              Together, We Can Protect Every Child
-            </h1>
-            <p>
-              Partner with us to eradicate child exploitation and create a world
-              where every child can grow up safe, empowered, and loved.
-              Together, we can give children the future they deserve.
-            </p>
-            <div>
-              <Link href={'/membership'} className={buttonVariants()}>
-                Join the Movement
-              </Link>
-              <Link href={'/donations'} className={buttonVariants()}>
-                Make a Donation
-              </Link>
-            </div>
-          </div>
+    <section>
+      <div className="container p-20 flex items-center justify-between">
+        <div className="max-w-lg">
+          <h1 className="text-5xl text-pretty font-bold">
+            Together, We Can Protect Every Child
+          </h1>
+          <p>
+            Partner with us to eradicate child exploitation and create a world
+            where every child can grow up safe, empowered, and loved. Together,
+            we can give children the future they deserve.
+          </p>
           <div>
-            <Image src="/hero.jpg" width={400} height={500} alt="" />
+            <Link href={'/membership'} className={buttonVariants()}>
+              Join the Movement
+            </Link>
+            <Link href={'/donations'} className={buttonVariants()}>
+              Make a Donation
+            </Link>
           </div>
         </div>
-      </section>
-    </>
+        <div>
+          <Image src="/hero.jpg" width={400} height={500} alt="" />
+        </div>
+      </div>
+    </section>
   )
 }
