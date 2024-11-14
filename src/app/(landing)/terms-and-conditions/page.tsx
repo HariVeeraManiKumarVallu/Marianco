43d97de45cdc65f6f9131b57a340db6d94c7e23d
FaizanAsifB <faizanasif.dev@gmail.com>
2024-11-14 11:01:24 +0100

    add terms page

---
+++ src/app/(landing)/terms-and-conditions/page.tsx
@@ -0,0 +1,231 @@
+import TitleSection from '@/components/title-section'
+
+export default function Page() {
+  return (
+    <div className="space-y-12 container">
+      {/* <TitleSection
+        title="Terms &amp; Conditions"
+        description=""
+        image={{
+          url: '/images/hero.jpg',
+          altText: 'Marianco',
+        }}
+      /> */}
+      <header className="space-y-4 mt-32">
+        <h1>Terms &amp; Conditions</h1>
+        <p>
+          Welcome to Marianco. These Terms &amp; Conditions (&quot;Terms&quot;)
+          govern your access to and use of Marianco&apos;s website, resources,
+          and services. By accessing or using any of Marianco&apos;s platforms
+          or participating in any Marianco programs, you agree to comply with
+          and be bound by these Terms. If you do not agree with these Terms,
+          please refrain from using our services or engaging in our programs.
+        </p>
+      </header>
+
+      <div className="space-y-2">
+        <h2>1. Organization Overview</h2>
+        <p>
+          Marianco is a non-governmental, non-profit organization dedicated to
+          protecting the safety, rights, and well-being of children around the
+          world. Our mission is to foster safe, nurturing environments for
+          children in all regions by providing resources, advocacy, and
+          community support. Marianco works in partnership with other
+          organizations, local governments, and communities to carry out its
+          mission.
+        </p>
+      </div>
+
+      <div className="space-y-2">
+        <h2>2. Eligibility and Participation</h2>
+        <p>
+          Marianco&apos;s services are open to individuals, families,
+          volunteers, and partner organizations who align with our mission to
+          protect children globally. By participating in any of our programs or
+          services, you confirm that you:
+        </p>
+        <ul>
+          <li>
+            Are 18 years of age or older, or have consent from a legal guardian
+            if you are a minor.
+          </li>
+          <li>
+            Agree to follow all Marianco&apos;s policies, guidelines, and these
+            Terms while participating in our programs.
+          </li>
+          <li>
+            Acknowledge that any activities with Marianco are at your own
+            discretion, and Marianco is not responsible for any personal
+            outcomes that may result from participating in these activities.
+          </li>
+        </ul>
+      </div>
+
+      <div className="space-y-4">
+        <div className="space-y-2">
+          <h2>3. Privacy and Child Protection</h2>
+          <p>
+            Marianco is committed to the privacy and safety of all children
+            involved in our programs. We abide by strict policies to protect the
+            data and identities of children and families.
+          </p>
+        </div>
+
+        <div>
+          <h3>Data Collection</h3>
+          <p>
+            Marianco may collect personal information necessary to carry out our
+            services. All data will be handled in compliance with global data
+            protection standards, including the GDPR (General Data Protection
+            Regulation) for individuals in the EU.
+          </p>
+        </div>
+
+        <div>
+          <h3>Use of Information</h3>
+          <p>
+            Marianco uses information provided by participants solely to deliver
+            our services and resources, improve our programs, and for internal
+            reporting and analysis.
+          </p>
+        </div>
+
+        <div>
+          <h3>Child Protection Standards</h3>
+          <p>
+            Marianco requires all staff, volunteers, and partners to follow
+            strict child protection policies, which include guidelines on
+            safeguarding, privacy, and reporting any incidents of abuse or
+            neglect.
+          </p>
+        </div>
+      </div>
+      <div className="space-y-2">
+        <h2>4. Community Conduct</h2>
+        <p>
+          We expect all participants, volunteers, and partners to behave in a
+          respectful and responsible manner that aligns with Marianco&apos;s
+          values. The following behaviors are strictly prohibited:
+        </p>
+        <div className="ml-8">
+          <ul className="list-disc">
+            <li>
+              Harassment, discrimination, or abuse of any individual, especially
+              children.
+            </li>
+            <li>
+              Unauthorized collection or sharing of any child&apos;s personal
+              data or imagery.
+            </li>
+            <li>
+              Any behavior that poses harm or potential risk to the well-being
+              of children.
+            </li>
+            <li>Violations of applicable laws or regulations.</li>
+          </ul>
+        </div>
+        <p>
+          Violation of these conduct standards may result in termination of
+          participation and involvement with Marianco, and, if applicable,
+          notification to appropriate authorities.
+        </p>
+      </div>
+
+      <div className="space-y-2">
+        <h2>5. Use of Materials and Resources</h2>
+        <p>
+          Marianco provides various educational materials, resources, and online
+          content to support child protection initiatives globally. These
+          resources are for personal use only, and their reproduction,
+          redistribution, or commercialization without written permission from
+          Marianco is strictly prohibited.
+        </p>
+        <h3>Content Use</h3>
+        <p>
+          Content on Marianco&apos;s website, including images, text, graphics,
+          and videos, is owned by or licensed to Marianco and is protected by
+          copyright laws.
+        </p>
+        <h3>External Links</h3>
+        <p>
+          Marianco&apos;s website may contain links to third-party websites.
+          Marianco does not endorse or assume responsibility for the content,
+          privacy policies, or practices of any third-party websites.
+        </p>
+      </div>
+
+      <div className="space-y-2">
+        <h2>6. Donations and Fundraising</h2>
+        <p>
+          Marianco relies on the generosity of donors to carry out its mission.
+          All donations are voluntary and subject to the following:
+        </p>
+        <h3>Use of Funds</h3>
+        <p>
+          Donations are used to support Marianco&apos;s child protection
+          programs, including education, advocacy, and on-the-ground assistance.
+        </p>
+        <h3>Refund Policy</h3>
+        <p>
+          All donations are non-refundable unless a processing error occurs.
+        </p>
+        <h3>Acknowledgment of Donations</h3>
+        <p>
+          Marianco may publicly acknowledge donor contributions, but only with
+          the consent of the donor. Anonymous donations are also accepted.
+        </p>
+      </div>
+
+      <div className="space-y-2">
+        <h2>7. Limitation of Liability</h2>
+        <p>
+          Marianco and its affiliates, officers, employees, and agents are not
+          liable for any damages, losses, or injuries arising from participation
+          in our programs, use of our resources, or interactions with other
+          participants.
+        </p>
+        <h3>Third-Party Interactions</h3>
+        <p>
+          Marianco may collaborate with third-party organizations, but we do not
+          take responsibility for their actions or conduct.
+        </p>
+        <h3>No Warranties</h3>
+        <p>
+          Marianco&apos;s services are provided &quot;as is&quot; without
+          warranties of any kind, either express or implied, regarding the
+          accuracy or reliability of the information provided.
+        </p>
+      </div>
+
+      <div className="space-y-2">
+        <h2>8. Dispute Resolution and Governing Law</h2>
+        <p>
+          In the event of a dispute arising out of or relating to these Terms or
+          participation in Marianco&apos;s programs:
+        </p>
+        <h3>Dispute Resolution</h3>
+        <p>
+          We encourage informal dispute resolution. If a dispute cannot be
+          resolved informally, it shall be settled by binding arbitration in
+          [Insert Location].
+        </p>
+        <h3>Governing Law</h3>
+        <p>
+          These Terms are governed by the laws of [Insert Applicable
+          Jurisdiction], without regard to its conflict of law principles.
+        </p>
+      </div>
+
+      <div className="space-y-2">
+        <h2>9. Modifications to the Terms</h2>
+        <p>
+          Marianco reserves the right to modify these Terms at any time. Any
+          changes will be effective immediately upon posting the revised Terms
+          on our website. Continued participation in our programs or use of our
+          services after any modifications signifies your acceptance of the
+          revised Terms.
+        </p>
+      </div>
+    </div>
+  )
+}
