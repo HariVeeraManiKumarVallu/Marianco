9be8e079921628b2f7ef02fa81dfbd35ca3f8c3b
FaizanAsifB <faizanasif.dev@gmail.com>
2024-11-16 20:22:12 +0100

    added brand colors to tailwind.config

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-orange-500 text-primary-foreground hover:bg-orange-500/90',
+        default: 'bg-primary text-foreground hover:bg-primary/90',
         destructive:
           'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         outline:
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,13 +10,19 @@ export default {
   theme: {
     extend: {
       container: {
-        center: 'true',
+        center: true,
         padding: '2rem',
         screens: {
           xl: '1280px',
         },
       },
       colors: {
+        'brand-color-1': '#fdfefd',
+        'brand-color-2': '#daf7fa',
+        'brand-color-3': '#afd7e3',
+        'brand-color-4': '#70aec5',
+        'brand-color-5': '#4b85b8',
+        'brand-color-6': '#7cb58a',
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
         card: {
