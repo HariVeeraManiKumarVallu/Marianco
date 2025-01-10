ae45a8a755d597e1d024c4ba81cfc548b8502d2f
FaizanAsifB <faizanasif.dev@gmail.com>
2025-01-10 18:23:20 +0100

    add custom link to BlocksRenderer

--- src/components/content-renderer.tsx
+++ src/components/content-renderer.tsx
@@ -1,8 +1,14 @@
 'use client'
 import { BlocksContent, BlocksRenderer } from '@strapi/blocks-react-renderer'
+import Link from 'next/link'
 
 const ContentRenderer = ({ content }: { content: BlocksContent }) => {
-  return <BlocksRenderer content={content} />
+  return <BlocksRenderer content={content} blocks={{
+    link: ({ children, url }) => {
+      if (url.includes('https://marianco.org') || url.includes('www.marianco.org') || !url.includes('https')) return <Link href={url}>{children}</Link>
+      return <a href={url} target='_blank' rel="noopener">{children}</a>
+    }
+  }} />
 }
 
 export default ContentRenderer
